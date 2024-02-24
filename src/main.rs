de6cbfec2756a4632ffc0ce88e5cb0266dfe1500
fist_it <franekfabinski@gmail.com>
2024-02-24 10:31:43 +0100

    figured out actix tree for hrefs prepared for tailwind css

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,21 @@
-use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpRequest, HttpServer, Responder};
+use actix_files as fs;
+use fs::{NamedFile, Files};
 
 
 
 #[get("/")]
-async fn index(_req: HttpRequest) -> HttpResponse {
-    HttpResponse::Ok()
-        .content_type("text/html")
-        .body(include_str!("./views/index.html"))
+async fn index(_req: HttpRequest) -> impl Responder {
+    NamedFile::open_async("./views/index.html").await
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(index)
-            .route("/hey", web::get().to(manual_hello))
+            .service(Files::new("/css", "./views/static/css"))
     })
     .bind("127.0.0.1:8080")?
     .run()
