e03292a0b326811f9faa7c2afd3dab3532b760db
fist_it <franekfabinski@gmail.com>
2024-02-29 19:10:40 +0100

    tsx support, document root to /portfolio

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,9 @@ use fs::{NamedFile, Files};
 
 #[get("/")]
 async fn index(_req: HttpRequest) -> impl Responder {
-    NamedFile::open_async("../client/build/index.html").await
+    NamedFile::open_async("./client/build/index.html").await
 }
 
-#[get("/marcin")]
-async fn marcin(_req: HttpRequest) -> impl Responder {
-    NamedFile::open_async("./views/marcin/index.html").await
-}
 
 
 #[actix_web::main]
@@ -18,7 +14,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(index)
-            .service(Files::new("/", "../client/build/"))
+            .service(Files::new("/", "./client/build/"))
             .service(marcin)
     })
     .bind("127.0.0.1:8080")?
