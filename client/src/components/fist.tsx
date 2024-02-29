1328c381f59069db8b767ac053963a14a4df51e2
waszuch <140809966+waszuch@users.noreply.github.com>
2024-02-29 22:00:32 +0100

    fix

--- client/src/components/fist.tsx
+++ client/src/components/fist.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 function Counter({ title }: {title: string}) {
   let counter = 0;
   return(
-    <button onClick={counter = counter+1}>{counter}</button>
+    <button></button>
   );
 }
 
