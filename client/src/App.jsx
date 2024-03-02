0f91895032b148b337d5c04fd9da435575dd5a64
waszuch <140809966+waszuch@users.noreply.github.com>
2024-03-02 16:14:21 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,7 @@ import Counter from "./components/fist"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Counter />
-          Learn React pls
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
